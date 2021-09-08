2be784a210749a74a6294eb82d406cd578414772
Maria Mireles <mireles335@gmail.com>
2021-09-08 17:45:38 -0500

    minifying images

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,53 @@
-// const path = require('path');
-// const webpack = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-
-
-
-// module.exports = {entry: './assets/js/script.js', output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'main.bundle.js',
-//     plugins: [  new webpack.ProvidePlugin({
-//         $: "jquery",
-//         jQuery: "jquery"
-//       }),],
-//   }, mode: 'development'};
-
-const webpack = require("webpack")
+const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-  entry: "./assets/js/script",
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js",
+  },
+
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ],
+      },
+    ],
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
   ],
-  mode: "development"
+  mode: "development",
 };
