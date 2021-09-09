5882badb93b629bc4e8ff4a0d4be771659250374
Maria Mireles <mireles335@gmail.com>
2021-09-09 15:06:44 -0500

    testing localhost

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -2,18 +2,6 @@ const APP_PREFIX = 'FoodFest-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-
-self.addEventListener('install', function (e) {
-    self.addEventListener('install', function (e) {
-        e.waitUntil(
-          caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-          })
-        )
-      })
-
-})
 const FILES_TO_CACHE = [
     "./index.html",
     "./events.html",
@@ -27,3 +15,14 @@ const FILES_TO_CACHE = [
     "./dist/tickets.bundle.js",
     "./dist/schedule.bundle.js"
   ];
+
+
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+});
